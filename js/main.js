8fb856e9f9f5d26c15384abdc340357d5d3d5a97
Adam Jonze <mail@jonze.io>
2018-02-28 17:22:50 +0000

    html views etc

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+   var modal = $("#modal");
+   var modalOverlay = $("#modal-overlay");
+   var closeButton = $("#close-modal");
+   var openButton = $("#open-modal");
+   var ep = $(".e-text");
+   var e0 = "email: "
+   var e1 = "mail";
+   var e2 = "jonze";
+   var e3 = ".io";
+
+   $(function(){
+      $(".site-header").load("header.html");
+      $(".site-footer").load("footer.html");       
+    });
+
+   openButton.click(function() {
+     ep.append(e0 + e1 + "@" + e2 + e3);
+     modal.toggleClass("closed");
+     modalOverlay.toggleClass("closed");
+   });
+
+   closeButton.click(function() {
+     ep.empty();
+     modal.toggleClass("closed");
+     modalOverlay.toggleClass("closed");
+   });
+
+   modalOverlay.click(function() {
+     ep.empty();
+     modal.toggleClass("closed");
+     modalOverlay.toggleClass("closed");
+   });
+ });
